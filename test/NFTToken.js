b25331d48939ac8582216f79d89041a0e841de64
melbesnard85 <melbesnard85@gmail.com>
2021-03-08 22:22:28 +0200

    feat: transfers test were added

--- test/NFTToken.js
+++ test/NFTToken.js
@@ -31,55 +31,59 @@ describe("Token contract", function() {
             const tokenID = 1;
             await hardhatDMarketNFTSwap.mintToken(addr1.address, tokenID);
             const fullURI = await hardhatDMarketNFTSwap.tokenURI(tokenID);
-            expect(tokenPrefix+tokenID).to.equal(fullURI);
+            expect(tokenPrefix+tokenID).equal(fullURI);
         });
         it("should set the owner", async function() {
-            expect(owner.address).to.equal(await hardhatDMarketNFTSwap.owner());
+            expect(owner.address).equal(await hardhatDMarketNFTSwap.owner());
         });
         it("should set the mint_role for creator address", async function() {
-            expect(true).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, creator.address));
+            expect(true).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, creator.address));
         });
         it("owner isn't minter", async function() {
-            expect(false).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, owner.address));
+            expect(false).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, owner.address));
+        });
+        it("name and symbol", async function () {
+            expect("DMarket NFT Swap").equal(await hardhatDMarketNFTSwap.name());
+            expect("DM NFT").equal(await hardhatDMarketNFTSwap.symbol());
         });
     });
 
     describe("Mint", async function () {
         it("owner add Minter", async function (){
             await hardhatDMarketNFTSwap.addMinter(minter.address);
-            expect(true).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
+            expect(true).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
         });
         it("revokeMinter role", async function (){
             await hardhatDMarketNFTSwap.addMinter(minter.address);
-            expect(true).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
+            expect(true).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
             await hardhatDMarketNFTSwap.revokeMinter(minter.address)
-            expect(false).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
+            expect(false).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
         });
         it("renounceMinter role", async function () {
             await hardhatDMarketNFTSwap.addMinter(addr2.address);
-            expect(true).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, addr2.address));
+            expect(true).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, addr2.address));
             await hardhatDMarketNFTSwap.connect(addr2).renounceMinter(addr2.address);
-            expect(false).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
+            expect(false).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, minter.address));
         });
         it("renounceMinter role by owner", async function () {
             await hardhatDMarketNFTSwap.addMinter(addr2.address);
-            expect(true).to.equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, addr2.address));
+            expect(true).equal(await hardhatDMarketNFTSwap.hasRole(MINT_ROLE, addr2.address));
             let getError = false;
             try {
                 await hardhatDMarketNFTSwap.connect(owner).renounceMinter(addr2.address);
             } catch (e) {
                 getError = e.toString().includes("AccessControl: can only renounce roles for self");
             }
-            expect(true).to.equal(getError);
+            expect(true).equal(getError);
         });
         it("creator mintToken to addr1", async function () {
             await hardhatDMarketNFTSwap.mintToken(addr1.address, 1);
-            expect(addr1.address).to.equal(await hardhatDMarketNFTSwap.ownerOf(1));
+            expect(addr1.address).equal(await hardhatDMarketNFTSwap.ownerOf(1));
         });
         it("minter mintToken to addr2", async function () {
             await hardhatDMarketNFTSwap.addMinter(minter.address);
             await hardhatDMarketNFTSwap.connect(minter).mintToken(addr2.address, 2);
-            expect(addr2.address).to.equal(await hardhatDMarketNFTSwap.ownerOf(2));
+            expect(addr2.address).equal(await hardhatDMarketNFTSwap.ownerOf(2));
         });
         it("failed mintToken", async function () {
             let isFailed = false;
@@ -88,7 +92,7 @@ describe("Token contract", function() {
             } catch (e) {
                 isFailed = e.toString().includes("MinterAccess: Sender is not a minter")
             }
-            expect(true).to.equal(isFailed)
+            expect(true).equal(isFailed)
         });
         it("already minted token", async function() {
             await hardhatDMarketNFTSwap.mintToken(addr1.address, 10);
@@ -98,7 +102,78 @@ describe("Token contract", function() {
             } catch (e) {
                 alreadyMinted = e.toString().includes("ERC721: token already minted");
             }
-            expect(true).to.equal(alreadyMinted);
+            expect(true).equal(alreadyMinted);
+        });
+        it("mintTokenBatch", async function () {
+            await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [101, 102]);
+            expect(addr1.address).equal(await hardhatDMarketNFTSwap.ownerOf(101));
+            expect(addr2.address).equal(await hardhatDMarketNFTSwap.ownerOf(102));
+        });
+        it("mintTokenBatch must be the same number of receivers/tokenIDs", async function () {
+            let getError = false;
+            try {
+                await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address], [101, 102]);
+            } catch (e) {
+                getError = e.toString().includes("DMarketNFTToken: must be the same number of receivers/tokenIDs");
+            }
+            expect(true).equal(getError);
+            getError = false;
+            try {
+                await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [101]);
+            } catch (e) {
+                getError = e.toString().includes("DMarketNFTToken: must be the same number of receivers/tokenIDs");
+            }
+            expect(true).equal(getError);
+        });
+        it("mintTokenBatch token already minted", async function () {
+            await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [101, 102]);
+            let getError = false;
+            try {
+                await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [101, 102]);
+            } catch (e) {
+                getError = e.toString().includes("ERC721: token already minted");
+            }
+            expect(true).equal(getError);
+            getError = false;
+            try {
+                await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [101, 103]);
+            } catch (e) {
+                getError = e.toString().includes("ERC721: token already minted");
+            }
+            expect(true).equal(getError);
+            getError = false;
+            try {
+                await hardhatDMarketNFTSwap.mintTokenBatch([addr1.address, addr2.address], [201, 201]);
+            } catch (e) {
+                getError = e.toString().includes("ERC721: token already minted");
+            }
+            expect(true).equal(getError);
+        });
+    });
+    describe("transfers", async function (){
+        it("transferFrom", async function () {
+            await hardhatDMarketNFTSwap.mintToken(addr1.address, 11);
+            expect(addr1.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
+            await hardhatDMarketNFTSwap.connect(addr1).transferFrom(addr1.address, addr2.address, 11);
+            expect(addr2.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
+        });
+        it("safeTransferFrom without bytes", async function () {
+            await hardhatDMarketNFTSwap.mintToken(addr1.address, 11)
+            expect(addr1.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
+
+            const contract = await hardhatDMarketNFTSwap.connect(addr1);
+            await contract['safeTransferFrom(address,address,uint256)'](addr1.address, addr2.address, 11);
+
+            expect(addr2.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
+        });
+        it("safeTransferFrom with bytes", async function () {
+            await hardhatDMarketNFTSwap.mintToken(addr1.address, 11);
+            expect(addr1.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
+
+            const contract = await hardhatDMarketNFTSwap.connect(addr1);
+            await contract['safeTransferFrom(address,address,uint256,bytes)'](addr1.address, addr2.address, 11, [0, 1]);
+
+            expect(addr2.address).equal(await hardhatDMarketNFTSwap.ownerOf(11));
         });
     });
 });
